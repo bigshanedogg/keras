5f400ac2c1dc501d7a7acac51bab303c255b215f
bigshanedogg <bigshane319@gmail.com>
2019-10-22 00:27:54 +0900

    Update variational_recurrent_autoencoder.py

--- examples/variational_recurrent_autoencoder.py
+++ examples/variational_recurrent_autoencoder.py
@@ -44,67 +44,6 @@ def sampling(args):
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
-
-def plot_results(models,
-                 data,
-                 batch_size=128,
-                 model_name="vae_mnist"):
-    """Plots labels and MNIST digits as a function of the 2D latent vector
-    # Arguments
-        models (tuple): encoder and decoder models
-        data (tuple): test data and label
-        batch_size (int): prediction batch size
-        model_name (string): which model is using this function
-    """
-
-    encoder, decoder = models
-    x_test, y_test = data
-    os.makedirs(model_name, exist_ok=True)
-
-    filename = os.path.join(model_name, "vae_mean.png")
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(x_test,
-                                   batch_size=batch_size)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
-    plt.colorbar()
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.savefig(filename)
-    plt.show()
-
-    filename = os.path.join(model_name, "digits_over_latent.png")
-    # display a 30x30 2D manifold of digits
-    n = 30
-    digit_size = 28
-    figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = np.linspace(-4, 4, n)
-    grid_y = np.linspace(-4, 4, n)[::-1]
-
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[i * digit_size: (i + 1) * digit_size,
-                   j * digit_size: (j + 1) * digit_size] = digit
-
-    plt.figure(figsize=(10, 10))
-    start_range = digit_size // 2
-    end_range = (n - 1) * digit_size + start_range + 1
-    pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range_x = np.round(grid_x, 1)
-    sample_range_y = np.round(grid_y, 1)
-    plt.xticks(pixel_range, sample_range_x)
-    plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.imshow(figure, cmap='Greys_r')
-    plt.savefig(filename)
-    plt.show()
-
 # IMDB dataset
 max_features = 20000
 # cut texts after this number of words
@@ -130,7 +69,7 @@ latent_dim = 256
 batch_size = 512
 epochs = 50
 
-# VAE model = encoder + decoder
+# VRAE model = encoder + decoder
 # build encoder model
 inputs = Input(shape=input_shape, name='encoder_inputs')
 embedding_layer = Embedding(max_features, embed_dim, input_length=maxlen, trainable=True)
@@ -146,7 +85,6 @@ z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
 # instantiate encoder model
 encoder = Model(inputs, [z_mean, z_log_var, z, h, c], name='encoder')
 encoder.summary()
-plot_model(encoder, to_file='vrae_encoder.png', show_shapes=True)
 
 # build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='z')
@@ -160,7 +98,6 @@ outputs = wrappers.TimeDistributed(Dense(embed_dim))(x)
 # instantiate decoder model
 decoder = Model([latent_inputs, h, c], outputs, name='decoder')
 decoder.summary()
-plot_model(decoder, to_file='vrae_decoder.png', show_shapes=True)
 
 # instantiate VAE model
 outputs = decoder(encoder(inputs)[2:])
@@ -185,9 +122,6 @@ if __name__ == '__main__':
     vrae.add_loss(vrae_loss)
     vrae.compile(optimizer='adam')
     vrae.summary()
-    plot_model(vrae,
-               to_file='vrae.png',
-               show_shapes=True)
 
     if args.weights:
         vrae.load_weights(args.weights)
@@ -198,8 +132,3 @@ if __name__ == '__main__':
                 batch_size=batch_size,
                 validation_data=(x_test, None))
         vrae.save_weights('vrae_mlp_mnist.h5')
-
-        plot_results(models,
-                     data,
-                     batch_size=batch_size,
-                     model_name="vrae")
